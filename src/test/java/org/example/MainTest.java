ac269bc56fb38988a4643a6c66b3710167427dee
Yuliia Bezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-07-01 10:14:44 +0200

    Refactor Warnings

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,19 +1,17 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.example.Main.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
-  @ParameterizedTest
+    @ParameterizedTest
     @CsvSource({"Qwertyu, false",
             "Qwertyui12, true",
             "1234567, false",
             "A1S2d3F4f5, true"})
-   void istPasswordMindenstens8Zeichen(String password, boolean expected) {
+    void istPasswordMindenstens8Zeichen(String password, boolean expected) {
         assertEquals(expected, Main.isPasswordLong(password));
     }
 
